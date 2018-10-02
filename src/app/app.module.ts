b29be67d7e63840392c18055fe785084b21a5a35
Barby87 <bcaceres.sepulveda@hotmail.com>
2018-10-02 09:14:20 -0300

    Agragando componentes home y options-service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,24 @@ import { NgModule } from '@angular/core'
 import { AppComponent } from './app.component';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {OptionsServiceComponent} from './components/options-service/options-service.component';
+import { HomeComponent } from './components/home/home.component';
 
+/*const routes: Routes = [
+  { path: 'options', 
+    component: OptionsServiceComponent 
+  },
+  
+  { path: 'home',
+    component: HomeComponent
+  }
+];*/
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OptionsServiceComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
